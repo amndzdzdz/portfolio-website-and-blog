0ca5c9045a7dfbf157356c974d20ea23d5e33726
amndzdzdz <zorrofx@gmx.net>
2025-10-26 10:00:11 +0100

    implemented createBlogPost controller

--- backend/src/controllers/blogPostController.ts
+++ backend/src/controllers/blogPostController.ts
@@ -1,16 +1,45 @@
+import { Request, Response } from "express";
 import asyncHandler = require("express-async-handler");
+const BlogPost = require("../models/blogPostModel");
 
 //@desc Create one blog post
 //@route POST /blogs/
 //@access public
-const createBlogPost = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Created one blog post" });
+const createBlogPost = asyncHandler(async (req: Request, res: Response) => {
+  const { title, caption, category, date, timeToRead, image, content } =
+    req.body;
+  if (
+    !title ||
+    !caption ||
+    !category ||
+    !date ||
+    !timeToRead ||
+    !image ||
+    !content
+  ) {
+    res.status(400);
+    throw new Error("All fields to createa  blog-post are required!");
+  }
+  const createdBlogPost = await BlogPost.create({
+    title: title,
+    caption: caption,
+    category: category,
+    date: date,
+    timeToRead: timeToRead,
+    image: image,
+    content: content,
+  });
+  if (!createBlogPost) {
+    res.status(400);
+    throw new Error("An error occured while creating a blog-post");
+  }
+  res.status(200).json({ blogPost: createdBlogPost });
 });
 
 //@desc Get one blog post
 //@route GET /blogs/:id
 //@access public
-const getBlogPostById = asyncHandler(async (req, res) => {
+const getBlogPostById = asyncHandler(async (req: Request, res: Response) => {
   const id = req.params.id;
   res.status(200).json({ message: `Get blog with id: ${id}` });
 });
@@ -18,7 +47,7 @@ const getBlogPostById = asyncHandler(async (req, res) => {
 //@desc Update one blog post
 //@route PUT /blogs/
 //@access public
-const updateBlogPost = asyncHandler(async (req, res) => {
+const updateBlogPost = asyncHandler(async (req: Request, res: Response) => {
   console.log(req.body);
   res.status(200).json({ message: "Updated one blog post" });
 });
@@ -26,14 +55,14 @@ const updateBlogPost = asyncHandler(async (req, res) => {
 //@desc Get all blog post
 //@route GET /blogs/
 //@access public
-const getBlogPosts = asyncHandler(async (_req, res) => {
+const getBlogPosts = asyncHandler(async (_req: Request, res: Response) => {
   res.status(200).json({ message: "Get all blogs" });
 });
 
 //@desc Delete a blog post
 //@route DELETE /blogs/:id
 //@access public
-const deleteBlogPost = asyncHandler(async (req, res) => {
+const deleteBlogPost = asyncHandler(async (req: Request, res: Response) => {
   console.log(req.body);
   res.status(200).json({ message: "Delete one blog post" });
 });
