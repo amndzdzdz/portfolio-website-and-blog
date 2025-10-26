2df290b91429ccf5199dcb67a95734d5b63eb91b
amndzdzdz <zorrofx@gmx.net>
2025-10-26 07:27:43 +0100

    added the mongoose blogPost model

---
+++ backend/src/models/blogPostModel.ts
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const blogPostSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "A title is required!"],
+    },
+    caption: {
+      type: String,
+      required: [true, "A caption is required!"],
+    },
+    date: {
+      type: Date,
+      required: [true, "A date is required!"],
+    },
+    timeToRead: {
+      type: Number,
+      required: [true, "A time to read is required!"],
+    },
+    image: {
+      type: String,
+      required: [true, "An image is required!"],
+    },
+    content: {
+      type: String,
+      required: [true, "The content is required!"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("blogPost", blogPostSchema);
