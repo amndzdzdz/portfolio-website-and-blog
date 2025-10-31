4573dce9ba277d4925d743a1797e8bfc3efab1af
amndzdzdz <zorrofx@gmx.net>
2025-10-31 20:40:45 +0100

    refactored codebase and deleted some files

---
+++ ui/src/app/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Hero></Hero>
+      <Body></Body>
+      <CommentSection></CommentSection>
+    </>
+  );
+}
+
+export default App;
