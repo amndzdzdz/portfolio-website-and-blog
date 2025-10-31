d025d3383b2bae433ea014c1e146391a30f6f2e7
amndzdzdz <zorrofx@gmx.net>
2025-10-31 20:50:21 +0100

    added high-level skeleton of the main page

--- ui/src/app/App.tsx
+++ ui/src/app/App.tsx
@@ -1,3 +1,8 @@
+import Header from "../features/header/Header";
+import Body from "../features/body/Body";
+import CommentSection from "../features/commentSection/CommentSection";
+import Hero from "../features/hero/Hero";
+
 function App() {
   return (
     <>
