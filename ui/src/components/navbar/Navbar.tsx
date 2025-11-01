cc5c8c97d868b54fbdb6f17e38b0d68aa99e7624
amndzdzdz <zorrofx@gmx.net>
2025-11-01 21:02:14 +0100

    added simple navbar

--- ui/src/components/navbar/Navbar.tsx
+++ ui/src/components/navbar/Navbar.tsx
@@ -1,3 +1,19 @@
 export default function Navbar() {
-  return <div className="content-center bg-amber-700">asdf</div>;
+  return (
+    <div
+      className="
+    grid grid-cols-2 gap-4 
+    top-0 fixed left-0 w-screen 
+    bg-[#0f2a4f] text-white p-4
+    "
+    >
+      <div></div>
+      <div className="flex justify-evenly items-center">
+        {" "}
+        <a className="col-span-1 hover: px-3">Contact</a>
+        <a className="col-span-1 px-3">About me</a>
+        <a className="col-span-1 px-3">Blog</a>
+      </div>
+    </div>
+  );
 }
