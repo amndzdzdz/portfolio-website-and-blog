d41c17d92bdbcdcf2586ef2f083e0e5febe4397f
amndzdzdz <zorrofx@gmx.net>
2025-11-02 20:45:44 +0100

    added my brothers name to navbar and changed to one col on small screens

--- ui/src/components/navbar/Navbar.tsx
+++ ui/src/components/navbar/Navbar.tsx
@@ -2,17 +2,16 @@ export default function Navbar() {
   return (
     <div
       className="
-    grid grid-cols-2 gap-4 
+    grid grid-cols-1 md:grid-cols-2 gap-4
     top-0 fixed left-0 w-screen 
     bg-[#0f2a4f] text-white p-4
     "
     >
-      <div></div>
+      <div className="hidden md:flex pl-4">Joseph Dziri</div>
       <div className="flex justify-evenly items-center">
-        {" "}
-        <a className="col-span-1 hover: px-3">Contact</a>
-        <a className="col-span-1 px-3">About me</a>
-        <a className="col-span-1 px-3">Blog</a>
+        <a className="col-span-1 underline underline-offset-3 px-3">Contact</a>
+        <a className="col-span-1 underline underline-offset-3 px-3">About me</a>
+        <a className="col-span-1 underline underline-offset-3px-3">Blog</a>
       </div>
     </div>
   );
