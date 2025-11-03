6a7f1dd6cd2bbb493463aecb970f7a7c56e3e92a
amndzdzdz <zorrofx@gmx.net>
2025-11-03 20:34:24 +0100

    added bouncing arrow to hero section

--- ui/src/components/hero/Hero.tsx
+++ ui/src/components/hero/Hero.tsx
@@ -36,6 +36,23 @@ export default function Hero() {
               className="sm:w-50 md:w-120 rounded-full shadow-lg"
             />
           </div>
+
+          <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="white"
+              className="w-8 h-8 animate-bounce"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </div>
         </div>
       </section>
     </section>
