f93c7da71e1f743fbffedd7e652b855d63de2c87
amndzdzdz <zorrofx@gmx.net>
2025-11-06 20:10:01 +0100

    completed EducationSectionItem component

--- ui/src/components/education/EducationSectionItem.tsx
+++ ui/src/components/education/EducationSectionItem.tsx
@@ -2,20 +2,24 @@ import type { EducationSectionItemType } from "../navbarElement/EducationSection
 
 export default function EducationSectionItem(props: EducationSectionItemType) {
   return (
-    <>
-      <div className="flex items-center justify-between">
-        <div className="flex-1 pr-8 text-right">
-          <h2 className="text-xl font-semibold">{props.uniName}</h2>
-          <p className="text-gray-600 dark:text-gray-300">{props.text}</p>
-        </div>
-        <div className="flex-1 flex justify-start pl-8">
-          <img
-            src={props.image}
-            alt="Technische Hochschule Ingolstadt"
-            className="w-24 h-24 rounded-full object-cover shadow-md"
-          />
+    <div className="flex flex-col md:flex-row items-center justify-between gap-6 md:gap-0">
+      <div className="flex-1 pr-0 md:pr-8 flex justify-center md:justify-end">
+        <div className="w-full md:w-4/4">
+          <h2 className="text-xl font-semibold text-center md:text-left">
+            {props.uniName}
+          </h2>
+          <p className="text-gray-600 dark:text-gray-300 text-center md:text-left">
+            {props.text}
+          </p>
         </div>
       </div>
-    </>
+      <div className="flex-1 flex justify-center md:justify-start pl-0 md:pl-8">
+        <img
+          src={props.image}
+          alt={props.uniName}
+          className="w-32 h-32 md:w-30 md:h-30 object-cover shadow-md"
+        />
+      </div>
+    </div>
   );
 }
