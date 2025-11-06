93c85c5c41ea64272551b82a52a5d17c523304de
amndzdzdz <zorrofx@gmx.net>
2025-11-06 20:23:05 +0100

    added initial contact section

---
+++ ui/src/components/contact/Contact.tsx
@@ -0,0 +1,16 @@
+export default function Contact() {
+  return (
+    <section className="bg-linear-to-r from-blue-600 to-indigo-700 text-white">
+      <section className="min-h-screen bg-linear-to-r from-blue-600 to-indigo-700 text-white flex items-center">
+        <div className="max-w-7xl mx-auto px-6 py-12 flex flex-col-reverse md:flex-row items-center gap-12 w-full">
+          <div className="w-full md:w-1/2 text-center md:text-left">
+            <h1 className="text-4xl md:text-6xl font-extrabold mb-6 leading-tight">
+              Get in contact with me!
+            </h1>
+          </div>
+          Here should be the formula
+        </div>
+      </section>
+    </section>
+  );
+}
