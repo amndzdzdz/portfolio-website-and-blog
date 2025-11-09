b67ef50fb67bdf296761c492b56af472eb695d8f
amndzdzdz <zorrofx@gmx.net>
2025-11-09 20:25:58 +0100

    combined the two types into one InformationItem type

--- ui/src/features/experienceSection/types/experienceItemType.ts
+++
@@ -1,6 +0,0 @@
-export type ExperienceItemContent = {
-  image: string;
-  title: string;
-  date: string;
-  bulletpoints: string[];
-};
--- ui/src/types/InformationItem.ts
+++ ui/src/types/InformationItem.ts
@@ -1,4 +1,4 @@
-export type EducationSectionItemType = {
+export type InformationItem = {
   image: string;
   uniName: string;
   text: string;
