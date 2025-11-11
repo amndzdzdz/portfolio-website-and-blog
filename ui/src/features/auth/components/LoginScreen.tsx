28fe4af32b1667ca710712fcb6806e991edc8c44
amndzdzdz <zorrofx@gmx.net>
2025-11-11 20:21:34 +0100

    added simple Login screen with no form yet

---
+++ ui/src/features/auth/components/LoginScreen.tsx
@@ -0,0 +1,9 @@
+export default function LoginScreen() {
+  return (
+    <section className="min-h-screen bg-linear-to-r from-blue-600 to-indigo-700">
+      <div className="flex min-h-screen justify-center items-center">
+        Hello World!
+      </div>
+    </section>
+  );
+}
