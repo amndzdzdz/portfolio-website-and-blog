28e744ec78fe7806e7d9e153d34294282c65fbfc
amndzdzdz <zorrofx@gmx.net>
2025-11-12 21:34:33 +0100

    added empty blog section

---
+++ ui/src/features/blog/components/blogSection/BlogSection.tsx
@@ -0,0 +1,11 @@
+export default function BlogSection() {
+  return (
+    <section
+      className="
+        min-h-screen bg-gray-50 mt-12 sm:mt-0 text-gray-800
+      "
+    >
+      <div id="content"></div>
+    </section>
+  );
+}
