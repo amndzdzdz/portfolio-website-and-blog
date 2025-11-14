a2dddb8785a323066d1f33c069b5b1b289628776
amndzdzdz <zorrofx@gmx.net>
2025-11-14 21:40:51 +0100

    created ScrollToTop component that scrolls to the top when routing

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./features/navbar/components/Navbar";
 import LandingPage from "./pages/LandingPage";
 import Blog from "./pages/Blog";
+import ScrollToTop from "./utils/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop></ScrollToTop>
       <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
