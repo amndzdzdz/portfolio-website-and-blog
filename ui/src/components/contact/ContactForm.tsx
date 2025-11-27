2da5cc86ffebe8250c4abc8cc2c19d16a24a3291
amndzdzdz <zorrofx@gmx.net>
2025-11-27 09:04:17 +0100

    added web3forms to contactform to send emails

--- ui/src/components/contact/ContactForm.tsx
+++ ui/src/components/contact/ContactForm.tsx
@@ -1,13 +1,44 @@
 import { useState } from 'react';
 import type { FormProps } from '../../types/FormProps';
+const web3formsKey = import.meta.env.VITE_WEB3FORMS_KEY;
 
 export default function ContactForm(formProps: FormProps) {
   const [isEmailEmpty, setIsEmailEmpty] = useState(true);
   const [isInputEmpty, setIsInputEmpty] = useState(true);
+  const [_result, setResult] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    setResult('');
+
+    const formData = new FormData(e.currentTarget);
+
+    formData.append('access_key', web3formsKey);
+
+    const response = await fetch('https://api.web3forms.com/submit', {
+      method: 'POST',
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setIsEmailEmpty(true);
+      setIsInputEmpty(true);
+    } else {
+      setResult('Something went wrong. Please try again.');
+    }
+
+    setLoading(false);
+    window.location.reload();
+  };
 
   return (
     <div className="w-full max-w-md mx-auto">
       <form
+        onSubmit={onSubmit}
         className="
            bg-white text-black shadow-lg rounded-2xl px-6 py-8 mb-4
       "
@@ -15,6 +46,7 @@ export default function ContactForm(formProps: FormProps) {
         <h1 className="text-2xl font-extrabold mb-6 text-center">
           {formProps.title}
         </h1>
+
         <div className="mb-4">
           <label
             className="block text-gray-800 text-sm font-semibold mb-2"
@@ -24,62 +56,56 @@ export default function ContactForm(formProps: FormProps) {
           </label>
           <input
             id="email"
+            name="email"
             type="email"
-            onChange={(e) => {
-              if (e.target.value.length > 0) {
-                setIsEmailEmpty(false);
-              } else {
-                setIsEmailEmpty(true);
-              }
-            }}
+            onChange={(e) => setIsEmailEmpty(e.target.value.length === 0)}
             placeholder="email"
             className="
               shadow appearance-none border rounded w-full 
               py-2 px-3 text-gray-700 leading-tight focus:outline-none 
               focus:ring-2 focus:ring-blue-500
             "
+            required
           />
         </div>
+
         <div className="mb-4">
           <label
             className="block text-gray-800 text-sm font-semibold mb-2"
-            htmlFor="text"
+            htmlFor="message"
           >
             Your message
           </label>
           <textarea
-            id="text"
-            onChange={(e) => {
-              if (e.target.value.length > 0) {
-                setIsInputEmpty(false);
-              } else {
-                setIsInputEmpty(true);
-              }
-            }}
+            id="message"
+            name="message"
+            onChange={(e) => setIsInputEmpty(e.target.value.length === 0)}
             placeholder="What do you want to tell me?"
             className="
               shadow appearance-none border rounded w-full py-2 px-3 
               text-gray-700 leading-tight h-40 resize-none focus:outline-none 
               focus:ring-2 focus:ring-blue-500
             "
+            required
           />
         </div>
+
         <div className="flex justify-center gap-4">
           <button
             type="submit"
-            disabled={isEmailEmpty || isInputEmpty}
+            disabled={isEmailEmpty || isInputEmpty || loading}
             className={`
               bg-blue-500 hover:bg-blue-600 text-white 
               font-bold py-2 px-4 rounded
               hover:cursor-pointer
               ${
-                isInputEmpty || isEmailEmpty
-                  ? 'disabled:cursor-not-allowed disabled:bg-gray-400'
+                isInputEmpty || isEmailEmpty || loading
+                  ? 'cursor-not-allowed bg-gray-400 hover:bg-gray-400'
                   : ''
               }
             `}
           >
-            Send
+            {loading ? 'Sending...' : 'Send'}
           </button>
         </div>
       </form>
