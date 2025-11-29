b63cd03be0b80064d05cc625926fc2d512c3d66d
amndzdzdz <zorrofx@gmx.net>
2025-11-29 21:32:43 +0100

    added getBlogPostById deleteBlogPost and updateBlogPost functions

--- ui/src/app/api/blogPostApi.ts
+++ ui/src/app/api/blogPostApi.ts
@@ -1,4 +1,4 @@
-import type { BlogPost } from '../../types/BlogPost';
+import type { BlogPost, BlogPostWithId } from '../../types/BlogPost';
 
 export async function createBlogPost(data: BlogPost) {
   const res = await fetch('http://localhost:8080/blogs/', {
@@ -28,5 +28,36 @@ export async function getBlogPostPreviews(limit: number, after?: string) {
       },
     },
   );
+  return blogPosts.json();
+}
+
+export async function getBlogPostById(id: string) {
+  const blogPosts = await fetch(`http://localhost:8080/blogs/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return blogPosts.json();
+}
+
+export async function deleteBlogPost(id: string) {
+  const blogPosts = await fetch(`http://localhost:8080/blogs/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return blogPosts.json();
+}
+
+export async function updateBlogPost(data: BlogPostWithId) {
+  const blogPosts = await fetch(`http://localhost:8080/blogs/`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
   return blogPosts.json();
 }
