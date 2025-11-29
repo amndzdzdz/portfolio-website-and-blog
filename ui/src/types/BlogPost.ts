1fb07ef205e7df35b842384232f7306a3954e9b9
amndzdzdz <zorrofx@gmx.net>
2025-11-29 14:26:51 +0100

    added BlogPostWithId type

--- ui/src/types/BlogPost.ts
+++ ui/src/types/BlogPost.ts
@@ -7,3 +7,14 @@ export type BlogPost = {
   image: string;
   content: string;
 };
+
+export type BlogPostWithId = {
+  id: string;
+  title: string;
+  caption: string;
+  category: string;
+  date: string;
+  timeToRead: string;
+  image: string;
+  content: string;
+};
