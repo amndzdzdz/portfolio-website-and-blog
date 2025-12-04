171feb9f8cfa503bea662ef920dfeee7499617e2
amndzdzdz <zorrofx@gmx.net>
2025-12-04 20:55:47 +0100

    created new commentform

--- ui/src/components/comment/Comment.tsx
+++ ui/src/components/comment/Comment.tsx
@@ -1,19 +1,22 @@
 import type { CommentProps } from '../../types/CommentProps';
 
 export default function Comment(commentProps: CommentProps) {
+  const formattedDate = new Intl.DateTimeFormat('de-DE').format(
+    new Date(commentProps.date),
+  );
   return (
     <div
-      id={commentProps.id}
+      id={commentProps.blogPostId}
       className="
             border border-gray-200
                 bg-white text-black shadow-xl rounded-xl px-6 py-8 mb-5 mt-5"
     >
       {' '}
       <div className="flex flex-row justify-between mb-5 ">
-        <p className="font-extrabold">{commentProps.author}</p>
-        <p className="flex justify-right">{commentProps.date}</p>
+        <p className="font-extrabold">{commentProps.name}</p>
+        <p className="flex justify-right">{formattedDate}</p>
       </div>
-      <p>{commentProps.text}</p>
+      <p>{commentProps.message}</p>
     </div>
   );
 }
---
+++ ui/src/components/comment/CommentForm.tsx
@@ -0,0 +1,98 @@
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { createComment } from '../../app/api/commentApi';
+
+export default function CommentForm() {
+  const { id } = useParams();
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const comment = {
+      blogPostId: id!,
+      name: name,
+      message: message,
+      date: new Date().toISOString(),
+    };
+    try {
+      await createComment(comment);
+    } catch (err) {
+      console.log('An error occured: ' + err);
+    }
+    window.location.reload();
+  };
+
+  return (
+    <div className="w-full max-w-md mx-auto">
+      <form
+        onSubmit={onSubmit}
+        className="
+           bg-white text-black shadow-lg rounded-2xl px-6 py-8 mb-4
+      "
+      >
+        <h1 className="text-2xl font-extrabold mb-6 text-center">
+          Write a comment!
+        </h1>
+
+        <div className="mb-4">
+          <label
+            className="block text-gray-800 text-sm font-semibold mb-2"
+            htmlFor="email"
+          >
+            Your name
+          </label>
+          <input
+            id="email"
+            name="email"
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            placeholder="name"
+            className="
+              shadow appearance-none border rounded w-full 
+              py-2 px-3 text-gray-700 leading-tight focus:outline-none 
+              focus:ring-2 focus:ring-blue-500
+            "
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            className="block text-gray-800 text-sm font-semibold mb-2"
+            htmlFor="message"
+          >
+            Your message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="What do you want to tell me?"
+            className="
+              shadow appearance-none border rounded w-full py-2 px-3 
+              text-gray-700 leading-tight h-40 resize-none focus:outline-none 
+              focus:ring-2 focus:ring-blue-500
+            "
+            required
+          />
+        </div>
+
+        <div className="flex justify-center gap-4">
+          <button
+            type="submit"
+            disabled={name.length > 0 && message.length > 0 ? false : true}
+            className={`
+              bg-blue-500 hover:bg-blue-600 text-white 
+              font-bold py-2 px-4 rounded
+              hover:cursor-pointer
+              disabled:cursor-not-allowed disabled:bg-gray-400
+            `}
+          >
+            Send
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
