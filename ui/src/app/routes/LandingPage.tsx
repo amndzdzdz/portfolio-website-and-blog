663a57f4794d1994af9b24ee8c30ef771383d44b
amndzdzdz <zorrofx@gmx.net>
2025-12-19 21:22:59 +0100

    changed scrollIntoView and removed Navbar

--- ui/src/app/routes/LandingPage.tsx
+++ ui/src/app/routes/LandingPage.tsx
@@ -4,24 +4,22 @@ import Contact from '../../components/contact/Contact';
 import EducationSection from '../../components/education/EducationSection';
 import ExperienceSection from '../../components/experience/ExperienceSection';
 import LandingPageHero from '../../components/hero/LandingPageHero';
-import Navbar from '../../components/navbar/Navbar';
 import { useEffect } from 'react';
 
 export default function LandingPage() {
   const { hash } = useLocation();
 
   useEffect(() => {
-    if (hash) {
-      const el = document.querySelector(hash);
-      if (el) {
-        el.scrollIntoView({ behavior: 'smooth' });
-      }
-    }
+    if (!hash) return;
+    const id = hash.replace('#', '');
+    requestAnimationFrame(() => {
+      const el = document.getElementById(id);
+      el?.scrollIntoView({ behavior: 'smooth' });
+    });
   }, [hash]);
 
   return (
     <>
-      <Navbar></Navbar>
       <LandingPageHero></LandingPageHero>
       <About></About>
       <ExperienceSection></ExperienceSection>
