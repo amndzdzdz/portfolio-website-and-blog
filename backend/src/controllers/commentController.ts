ac9c9a9de73ed42a356e241aa2f11175beb1ab42
amndzdzdz <zorrofx@gmx.net>
2025-12-23 19:58:01 +0100

    fixed some bugs in commentcontroller

--- backend/src/controllers/commentController.ts
+++ backend/src/controllers/commentController.ts
@@ -2,20 +2,20 @@ import { Request, Response } from 'express';
 import asyncHandler from 'express-async-handler';
 import Comment from '../models/commentModel';
 import BlogPost from '../models/blogPostModel';
+import mongoose from 'mongoose';
 
 //@desc Gets all comments for a given blogPost
 //@route GET /comments/:blogPostId
 //@access public
 export const getComments = asyncHandler(async (req: Request, res: Response) => {
   const id = req.params.id;
-  if (!id) {
+  if (!mongoose.Types.ObjectId.isValid(id!)) {
     res.status(400);
-    throw new Error('The blogpost id is missing!');
+    throw new Error('Invalid blog post ID');
   }
-  const blogPost = BlogPost.find({ _id: id });
+  const blogPost = await BlogPost.findById(id);
   if (!blogPost) {
-    res.status(404);
-    throw new Error('Blog post doesnt exist');
+    res.status(404).json({ message: 'Blog post does not exist' });
   }
   const comments = await Comment.find({ blogPostId: id });
   res.status(200).json({ comments: comments });
@@ -38,9 +38,9 @@ export const createComment = asyncHandler(
       throw new Error('The blogpost id is missing!');
     }
 
-    const blogPost = BlogPost.find({ _id: id });
+    const blogPost = await BlogPost.find({ _id: id });
     if (!blogPost) {
-      res.status(404);
+      res.status(400);
       throw new Error('Blog post doesnt exist');
     }
     const comment = await Comment.create({
